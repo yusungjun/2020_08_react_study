d0dbee53db15f33eea9a38931dfd9638c91747d4
sungjun <zzong1435@gmail.com>
2020-08-25 18:36:25 +0900

    love kimchi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+function Food({fav}){
+  console.log(fav);
+return <h2>I love {fav}</h2>
+}
+
 function App() {
   return (
     <div>
       <div className="App">
         <h1>Hello!</h1>
+        <Food 
+          fav="kimchi" 
+          prop1={true}
+          prop2={[1,"2",false]}
+          prop3={{
+            "key1":"value1"
+            ,"key2":"value2"
+          }}
+        />
+        <div className="hello"></div>
       </div>
     </div>
   );
