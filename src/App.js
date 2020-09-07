7bb646aae29b199c5ce21ec0255ff936297842d0
sungjun <zzong1435@gmail.com>
2020-09-07 19:08:33 +0900

    PropTypes study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
+import propTypes from "prop-types";
 
-function Food({fav}){
-  console.log(fav);
-return <h2>I love {fav}</h2>
+function Food({name, picture, rating}){
+  return <div>
+    <h2>I love {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} />
+  </div>
 }
+
+Food.propTypes = {
+  name : propTypes.string.isRequired,
+  picture : propTypes.string.isRequired,
+  rating : propTypes.number.isRequired
+}
+
+const foodILike = [
+  {
+    id : 1,
+    name : "take1",
+    image : "http://test.com/test1.jqg",
+    rating : 5
+  },
+  {
+    id : 2,
+    name : "take2",
+    image : "http://test.com/test2.jqg",
+    rating : 4.9
+  },
+  {
+    id : 3,
+    name : "take3",
+    image : "http://test.com/test3.jqg",
+    rating : 4.5
+  },
+];
+
 
 function App() {
   return (
     <div>
       <div className="App">
-        <h1>Hello!</h1>
-        <Food 
-          fav="kimchi" 
-          prop1={true}
-          prop2={[1,"2",false]}
-          prop3={{
-            "key1":"value1"
-            ,"key2":"value2"
-          }}
-        />
-        <div className="hello"></div>
+        {foodILike.map(obj =>(
+          <Food key={obj.id} name={obj.name} picture={obj.image} rating={obj.rating}/>
+        ))
+        }
       </div>
     </div>
   );
